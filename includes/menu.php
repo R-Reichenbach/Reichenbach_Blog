655bf08479efd316cbfc27bfe722b5f2ca5bb23f
R-Reichenbach <rafareichenbach@gmail.com>
2023-08-22 11:42:01 -0300

    modify

---
+++ includes/menu.php
@@ -0,0 +1,36 @@
+<div class="card">
+<div class="card-header">
+
+<div class="card-body">
+<ul class="nav flex-column">
+
+<li class="nav-item">
+
+<a class="nav-link" href="index.php">Home</a>
+</li>
+<li class="nav-item">
+
+<a class="nav-link" href="usuario formulario.php">Cadastre-se</a>
+</li>
+<li class="nav-item">
+
+<a class="nav-link" href="login formulario.php">Login</a>
+</li>
+<li class="nav-item">
+
+<a class="nav-link" href="post formulario.php">Incluir Post</a>
+</li>
+<?php if ((isset($_SESSION['login']))
+
+&& ($_SESSION['login'] ['usuario'] ['adm'] === 1)) : ?>
+
+<li class="nav-item">
+
+<a class="nav-link" href="usuarios.php">Usuários</a>
+</li>
+<?php endif; ?>
+
+</ul>
+
+</div>
+</div>
