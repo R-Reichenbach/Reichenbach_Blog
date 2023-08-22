ac8d28b1261e22c614ef8c589b1cd64118b40225
R-Reichenbach <rafareichenbach@gmail.com>
2023-08-22 11:32:45 -0300

    modify

---
+++ includes/topo.php
@@ -0,0 +1,18 @@
+<?php
+session start();
+?>
+
+<div class="card">
+ <div class="card-header">
+<hl> Projeto Blog em PHP + MySQL IFSP - RAFAEL REICHENBACH</hl>
+
+ </div>
+<?php if(isset($_SESSION['login'])): ?>
+ <div class="card-body text-right">
+
+Olá <?php echo $_SESSION['login']['usuario']['nome']?>!
+<a href="core/usuario_ repositorio.php?acac=logout"
+class="btn btn-link btn-sm" role="button">Sair</a>
+ </div>
+<?php endif ?>
+</div>
